a812cd1027975048fbe4494a6d9fe880d7cdc7e5
Houtamelo <antoniopedrogf@hotmail.com>
2026-08-13 20:01:05 -0300

    v.1.1.1 - Fix docs path

--- spire_enum/src/lib.rs
+++ spire_enum/src/lib.rs
@@ -1,14 +1,11 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 #![no_std]
 
 mod traits;
 
 pub mod prelude {
     pub use spire_enum_macros::{
-        delegate_impl,
-        delegated_enum,
-        discriminant_generic_table,
-        variant_generic_table,
+        delegate_impl, delegated_enum, discriminant_generic_table, variant_generic_table,
         variant_type_table,
     };
 
